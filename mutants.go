54f8e2971ed670c138ba58e720e6dc2f747c07ee
Camila Roji <camiroji@gmail.com>
2021-02-13 14:18:30 -0300

    Add isMutant function

---
+++ mutants.go
@@ -0,0 +1,93 @@
+package mutants
+
+import (
+	"strings"
+)
+
+func isMutant(dna []string) bool {
+	count := 0
+	var n = len(dna)
+	if n < 4 {
+		return false
+	}
+	diagonalVisited := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		diagonalVisited[i] = make([]bool, n)
+	}
+	verticalVisited := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		verticalVisited[i] = make([]bool, n)
+	}
+
+	for i, row := range dna {
+		hc, ok := checkHorizontalSequence(row)
+		count += hc
+		if ok || count > 1 {
+			return true
+		}
+		for j, _ := range row {
+			if !diagonalVisited[i][j] {
+				countDiagonal := 1
+				if ok := checkDiagonalSequence(countDiagonal, dna, diagonalVisited, i, j); ok {
+					count++
+				}
+				if count > 1 {
+					return true
+				}
+			}
+			if !verticalVisited[i][j] {
+				countVertical := 1
+				if ok := checkVerticalSequence(countVertical, dna, verticalVisited, i, j); ok {
+					count++
+				}
+				if count > 1 {
+					return true
+				}
+			}
+		}
+	}
+	return count > 1
+}
+
+func checkHorizontalSequence(row string) (int, bool) {
+	count := 0
+	if count += strings.Count(row, "AAAA"); count > 1 {
+		return count, true
+	}
+	if count += strings.Count(row, "TTTT"); count > 1 {
+		return count, true
+	}
+	if count += strings.Count(row, "CCCC"); count > 1 {
+		return count, true
+	}
+	count += strings.Count(row, "GGGG")
+	return count, count > 1
+}
+
+func checkDiagonalSequence(count int, adn []string, visited [][]bool, i int, j int) bool {
+	if count >= 4 {
+		return true
+	}
+	visited[i][j] = true
+	if i+1 < len(adn) && j+1 < len(adn) && !visited[i+1][j+1] && adn[i][j] == adn[i+1][j+1] {
+		count++
+		if ok := checkDiagonalSequence(count, adn, visited, i+1, j+1); ok {
+			return true
+		}
+	}
+	return false
+}
+
+func checkVerticalSequence(count int, adn []string, visited [][]bool, i int, j int) bool {
+	if count >= 4 {
+		return true
+	}
+	visited[i][j] = true
+	if i+1 < len(adn) && !visited[i + 1][j] && adn[i][j] == adn[i + 1][j] {
+		count++
+		if ok := checkVerticalSequence(count, adn, visited, i + 1,j); ok {
+			return true
+		}
+	}
+	return false
+}
---
+++ mutants_test.go
@@ -0,0 +1,122 @@
+package mutants
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCheckHorizontal(t *testing.T) {
+
+	t.Run("should find one sequence", func(t *testing.T) {
+		count, ok := checkHorizontalSequence("CCCCTA")
+		assert.Equal(t, false, ok)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("should find two sequences and return true", func(t *testing.T) {
+		count, ok := checkHorizontalSequence("CCCCTAGGGGA")
+		assert.Equal(t, true, ok)
+		assert.Equal(t, 2, count)
+	})
+
+	t.Run("should not find any sequence", func(t *testing.T) {
+		count, ok := checkHorizontalSequence("TCCCTAGGGCA")
+		assert.Equal(t, false, ok)
+		assert.Equal(t, 0, count)
+	})
+}
+
+func TestCheckVertical(t *testing.T) {
+	dna := []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}
+
+	t.Run("Should find a vertical sequence", func(t *testing.T) {
+		verticalVisited := make([][]bool, 6)
+		for i := 0; i < len(dna); i++ {
+			verticalVisited[i] = make([]bool, 6)
+		}
+		ok := checkVerticalSequence(1, dna, verticalVisited, 0, 4)
+		assert.Equal(t, true, ok)
+		assert.Equal(t, true, verticalVisited[2][4])
+		assert.Equal(t, false, verticalVisited[3][4])
+	})
+
+	t.Run("should not find a vertical sequence", func(t *testing.T) {
+		verticalVisited := make([][]bool, 6)
+		for i := 0; i < len(dna); i++ {
+			verticalVisited[i] = make([]bool, 6)
+		}
+		ok := checkVerticalSequence(1, dna, verticalVisited, 1, 4)
+		assert.Equal(t, false, ok)
+	})
+
+	t.Run("should not look two times in the same path", func(t *testing.T) {
+		verticalVisited := make([][]bool, 6)
+		for i := 0; i < len(dna); i++ {
+			verticalVisited[i] = make([]bool, 6)
+		}
+		checkVerticalSequence(1, dna, verticalVisited, 0, 4)
+		ok := checkVerticalSequence(1, dna, verticalVisited, 0, 4)
+		assert.Equal(t, false, ok)
+	})
+}
+
+
+func TestCheckDiagonal(t *testing.T) {
+	dna := []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}
+
+	t.Run("should find a diagonal sequence", func(t *testing.T) {
+		diagonalVisited := make([][]bool, 6)
+		for i := 0; i < len(dna); i++ {
+			diagonalVisited[i] = make([]bool, 6)
+		}
+		ok := checkDiagonalSequence(1, dna, diagonalVisited, 0, 0)
+		assert.Equal(t, true, ok)
+		assert.Equal(t, true, diagonalVisited[2][2])
+		assert.Equal(t, false, diagonalVisited[3][3])
+	})
+
+	t.Run("should not found a diagonal sequence", func(t *testing.T) {
+		diagonalVisited := make([][]bool, 6)
+		for i := 0; i < len(dna); i++ {
+			diagonalVisited[i] = make([]bool, 6)
+		}
+		ok := checkDiagonalSequence(1, dna, diagonalVisited, 1, 1)
+		assert.Equal(t, false, ok)
+	})
+
+	t.Run("should not look two times in the same diagonal", func(t *testing.T) {
+		diagonalVisited := make([][]bool, 6)
+		for i := 0; i < len(dna); i++ {
+			diagonalVisited[i] = make([]bool, 6)
+		}
+		checkDiagonalSequence(1, dna, diagonalVisited, 0, 0)
+		ok := checkDiagonalSequence(1, dna, diagonalVisited, 0, 0)
+		assert.Equal(t, false, ok)
+	})
+}
+
+func TestIsMutant(t *testing.T) {
+	t.Run("Check sequences of different letters", func(t *testing.T) {
+		dna := []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}
+		ok := isMutant(dna)
+		assert.Equal(t, true, ok)
+	})
+
+	t.Run("Check not mutant", func(t *testing.T) {
+		dna := []string{"ATGCGA", "CGGTGC", "TTATGT", "AGAATG", "TCCCTA", "TCACTG"}
+		ok := isMutant(dna)
+		assert.Equal(t, false, ok)
+	})
+
+	t.Run("Check mutant 3 sequences with overlapping letters", func(t *testing.T) {
+		dna := []string{"ATGCGA", "CGGTGC", "TTATGA", "AGAATA", "TCCCAA", "TCAAAA"}
+		ok := isMutant(dna)
+		assert.Equal(t, true, ok)
+	})
+
+	t.Run("Check mutant two sequences with overlapping letters", func(t *testing.T) {
+		dna := []string{"ATGCGA", "CGGTGC", "TTATGA", "AGACTA", "TCCCAA", "TCAAAA"}
+		ok := isMutant(dna)
+		assert.Equal(t, true, ok)
+	})
+}
