49195a732ee09304551a2d3a1b603fe03a439541
Camila Roji <camiroji@gmail.com>
2021-03-07 22:07:17 -0300

    Add validations

--- pkg/controller.go
+++ pkg/controller.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"regexp"
 )
 
 type Controller struct {
@@ -42,6 +43,11 @@ func (c Controller) VerifyDNA(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if !c.validateInput(dna.Dna) {
+		http.Error(w, "Invalid character or size", http.StatusBadRequest)
+		return
+	}
+
 	ok := IsMutant(dna.Dna)
 	err = c.DB.SaveDNA(Dna{dna: aws.String(strings.Join(dna.Dna, "")), isMutant: aws.Bool(ok)})
 	if err != nil {
@@ -55,6 +61,24 @@ func (c Controller) VerifyDNA(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Is Mutant"))
+}
+
+func (c Controller) validateInput(s []string) bool {
+	for _, d := range s {
+		if !c.validLetters(d) || !c.validSize(len(s), d) {
+			return false
+		}
+	}
+	return true
+}
+
+func (c Controller) validLetters(s string) bool {
+	hasInvalidLetters, err := regexp.MatchString("[^ATCG]", s)
+	return err == nil && !hasInvalidLetters
+}
+
+func (c Controller) validSize(n int, s string) bool {
+	return len(s) == n
 }
 
 func (c Controller) GetStats(w http.ResponseWriter, r *http.Request) {
--- pkg/controller_test.go
+++ pkg/controller_test.go
@@ -52,6 +52,51 @@ func TestController_VerifyDNA_notMutant(t *testing.T) {
 	assert.Equal(t, http.StatusForbidden, res.StatusCode)
 }
 
+func TestController_VerifyDNA_invalidCharacter(t *testing.T) {
+	c := Controller{DB: MockRepo{}}
+	s := httptest.NewServer(http.HandlerFunc(c.VerifyDNA))
+	defer s.Close()
+	client := s.Client()
+	body := bytes.NewBuffer([]byte(`
+{
+"dna":["ATGCGTA","CCGTGAA","TTAJGTA","AGAAGGG","CACCTAA","TCACTGA", "TCACTGA"]
+}
+`))
+	res, err := client.Post(s.URL+"/mutants", "application/json", body)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+}
+
+func TestController_VerifyDNA_invalidStringLen(t *testing.T) {
+	c := Controller{DB: MockRepo{}}
+	s := httptest.NewServer(http.HandlerFunc(c.VerifyDNA))
+	defer s.Close()
+	client := s.Client()
+	body := bytes.NewBuffer([]byte(`
+{
+"dna":["ATGCGTA","CCGTGAA","TTATTGTA","AGAAGGG","CACCTAA","TCACTGA", "TCACTGA"]
+}
+`))
+	res, err := client.Post(s.URL+"/mutants", "application/json", body)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+}
+
+func TestController_VerifyDNA_invalidArraySize(t *testing.T) {
+	c := Controller{DB: MockRepo{}}
+	s := httptest.NewServer(http.HandlerFunc(c.VerifyDNA))
+	defer s.Close()
+	client := s.Client()
+	body := bytes.NewBuffer([]byte(`
+{
+"dna":["ATGCGTA","CCGTGAA","TTATGTA","AGAAGGG","CACCTAA","TCACTGA", "TCACTGA", "AGAAGGG"]
+}
+`))
+	res, err := client.Post(s.URL+"/mutants", "application/json", body)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+}
+
 func TestController_GetStats(t *testing.T) {
 	c := Controller{DB: MockRepo{}}
 	s := httptest.NewServer(http.HandlerFunc(c.GetStats))
